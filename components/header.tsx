5631c9e730a24d1488eb5f4a9be4776583de8e7b
Suraj Mallick <surajhustlermallick@gmail.com>
2024-01-16 23:33:26 +0530

    added motion.li elements inside list for bounce effect of header/nav items on page load

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,13 @@
 
 import React from 'react';
 import {motion} from "framer-motion"
+import {links} from "@/lib/data";
+import Link from 'next/link';
+
+
+
+
+
 
 const header = () => {
   return (
@@ -21,6 +28,34 @@ const header = () => {
                 
 
         </motion.div>
+
+
+        <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+            <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+               { links.map( link=>(
+
+               <motion.li
+               initial={{y:-100 ,opacity:0}}
+               animate={{y:0,opacity:1}}
+               className=" h-3/4 flex items-center justify-center relative"
+                key={link.hash}
+                >
+
+                    <Link 
+                    className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300"
+                    href={link.hash}
+                    >
+
+                    {link.name}
+                    
+                    </Link>
+
+                </motion.li>
+                
+                ) 
+               ) }
+            </ul>
+        </nav>
 
     </header>
   )
