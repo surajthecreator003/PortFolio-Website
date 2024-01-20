2aad617eca15cf872ad434768333ff92abf9762d
Suraj Mallick <surajhustlermallick@gmail.com>
2024-01-20 23:51:26 +0530

    removed about

--- app/about/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const About = () => {
-  return (
-    <div> We are only using one route that is the home route as in Portfolio page we dont need multiple routes</div>
-  )
-}
-export default  About;
