c7f84d4899a6670adf6eb63941ac1c8d412e30aa
Suraj Mallick <121385784+surajthecreator003@users.noreply.github.com>
2024-01-22 20:04:51 +0530

    updated portfolio site link

--- lib/data.ts
+++ lib/data.ts
@@ -50,33 +50,6 @@ export const links = [
 
 
 
-//Add this if you have experience section
-// export const experiencesData = [
-//   {
-//     title: "Graduated bootcamp",
-//     location: "Miami, FL",
-//     description:
-//       "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-//     icon: React.createElement(LuGraduationCap),
-//     date: "2019",
-//   },
-//   {
-//     title: "Front-End Developer",
-//     location: "Orlando, FL",
-//     description:
-//       "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-//     icon: React.createElement(CgWorkAlt),
-//     date: "2019 - 2021",
-//   },
-//   {
-//     title: "Full-Stack Developer",
-//     location: "Houston, TX",
-//     description:
-//       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-//     icon: React.createElement(FaReact),
-//     date: "2021 - present",
-//   },
-// ] as const;
 
 
 export const certificationsData = [
@@ -128,7 +101,7 @@ export const projectsData = [
       " implemented Framer Motion for Amazing Animations and has features like Dark Mode,Sending Email ",
     tags: [ "Next.js","React.js","TypeScript", "RESEND for email", "Tailwind CSS", "Framer MOtion","Vercel-Deploy"],
     imageUrl: portfolioapp,
-    link:"https://port-folio-website-hkvlzfwnp-surajthecreator003.vercel.app/"
+    link:"https://port-folio-website-dusky.vercel.app/"
   },
 ] as const;
 
